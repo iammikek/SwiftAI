638c922c4bd0de41c02a3e1a068e5fba4a8e2316
Mike Karthauser <mike.karthauser@bookinglive.co.uk>
2023-07-19 16:49:47 +0100

    adds test for getting message count

--- tests/Feature/Controllers/ChatControllerTest.php
+++ tests/Feature/Controllers/ChatControllerTest.php
@@ -6,11 +6,59 @@ use App\Models\Conversation;
 use App\Models\Personality;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class ChatControllerTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @test */
+    public function test_can_send_message_returns_validation_errors()
+    {
+        $user = User::factory()->create();
+
+        $this
+            ->actingAs($user)
+            ->postJson('/send-message', [])->assertUnprocessable()
+            ->assertJson([
+                "conversation_id" => [
+                    "The conversation id field is required."
+                ],
+            ]);
+    }
+
+    public function test_can_send_message()
+    {
+        $user = User::factory()->create();
+        $conversation = Conversation::factory()->create(['user_id' => $user->id]);
+
+        Http::fake([
+            'https://api.openai.com/v1/chat/completions' => Http::response([
+                'choices' => [
+                    [
+                        'message' => [
+                            'content' => 'some reply',
+                        ]
+                    ]
+                ]
+            ]),
+        ]);
+
+        $this
+            ->actingAs($user)
+            ->postJson('/send-message', [
+                'conversation_id' => $conversation->id,
+                'message' => 'Some Message'
+            ])
+            ->assertSuccessful()
+            ->assertJson([
+                "reply" => 'some reply',
+                'messageCount' => 2,
+
+            ]);
+    }
+
 
     /** @test */
     public function test_can_create_new_conversation_fails_validation()
@@ -64,6 +112,20 @@ class ChatControllerTest extends TestCase
             ->assertSuccessful()
             ->assertJsonFragment($conversations->first()->toArray());
 
+    }
+
+    /** @test */
+    public function test_can_get_message_count()
+    {
+        $user = User::factory()->create();
+
+        $currentConversation = Conversation::factory()->create();
+
+        $this
+            ->actingAs($user)
+            ->getJson('/get-messages/' . $currentConversation->id . '/message-count')
+            ->assertSuccessful()
+            ->assertJsonStructure(['count']);
     }
 
     /** @test */
